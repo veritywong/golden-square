537ab86bfcedfa94fd30e49622ce7fa47c5033f1
Verity <veritywong90@gmail.com>
2023-04-12 15:42:04 +0100

    string builder spec updated

--- spec/string_builder_spec.rb
+++ spec/string_builder_spec.rb
@@ -13,11 +13,35 @@ RSpec.describe StringBuilder do
         expect(string_builder.output).to eq "5"
     end
 
-    context "given an addition of a string" do
+    context "given an addition of a string" do #context sets up a sub group
         it "outputs that string" do
+            string_builder = StringBuilder.new
+            string_builder.add("hello")
+            expect(string_builder.output).to eq "hello"
         end
 
         it "has a length of that string's length" do
+            string_builder = StringBuilder.new
+            string_builder.add("hello")
+            expect(string_builder.size).to eq 5
+        end
+    end
+
+    context "given many additions of a string" do
+        it "outputs that string" do
+            string_builder = StringBuilder.new
+            string_builder.add("hello")
+            string_builder.add(" there")
+            string_builder.add(" how are you?")
+            expect(string_builder.output).to eq "hello there how are you?"
+        end
+
+        it "has a length of that string's length" do
+            string_builder = StringBuilder.new
+            string_builder.add("hello")
+            string_builder.add(" there")
+            string_builder.add(" how are you?")
+            expect(string_builder.size).to eq 24
         end
     end
 
