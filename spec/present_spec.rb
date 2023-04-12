1b3c15f4d8658a099be5450cc19f60d2b91d7ed6
Verity <veritywong90@gmail.com>
2023-04-12 14:09:10 +0100

    present files added

--- spec/present_spec.rb
+++ spec/present_spec.rb
@@ -1,9 +1,20 @@
-require 'reminder'
+require 'present'
 
 RSpec.describe Present do
 
-    context "" do
-        it "" do
+    context "when content has already been wrapped" do
+        it "fails" do
+             present = Present.new
+             present.wrap("gift")
+             expect { present.wrap(1) }.to raise_error "A contents has already been wrapped."
         end
     end
+
+    context "when no contents have been wrapped" do
+        it "fails" do
+            present = Present.new
+            expect { present.unwrap }.to raise_error "No contents have been wrapped."
+        end
+    end
+
 end
